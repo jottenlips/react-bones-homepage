f7a167ceb95a9b51f61013bbb6978845a29dbf18
John Ottenlips <jottenlips@gmail.com>
2020-12-17 18:36:21 -0600

    bones test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import { Bones } from "react-bones/lib";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{}}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <img src={logo} className="App-logo" alt="logo" width={300} />
+        <h4
+          style={{
+            maxWidth: 300,
+            backgroundColor: "#000000",
+            borderRadius: 10,
+            padding: 5,
+          }}
         >
-          Learn React
-        </a>
+          {"💀 npm i react-bones 💀 "}
+        </h4>
+        <h5>{"Dead simple loading components for React and React-Native"}</h5>
+
+        <div
+          style={{
+            borderRadius: 6,
+            width: 400,
+            height: 200,
+            backgroundColor: "#ffffff",
+            padding: 24,
+          }}
+        >
+          <Bones borderRadius={25} />
+          <div style={{ padding: 10 }} />
+          <Bones width={"100%"} height={20} borderRadius={10} />
+          <div style={{ padding: 10 }} />
+          <Bones width={"100%"} height={20} borderRadius={10} />
+          <div style={{ padding: 10 }} />
+          <Bones width={"100%"} height={20} borderRadius={10} />
+          <div style={{ padding: 10 }} />
+        </div>
       </header>
     </div>
   );
